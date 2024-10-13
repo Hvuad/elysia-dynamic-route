614ca6cecfe313b77570b26f7251e4e7fe652814
hvuads <hvuadpeem@gmail.com>
2024-10-13 12:49:10 +0700

    Refactor ElysiaDynamicRoute to use class syntax

--- src/index.ts
+++ src/index.ts
@@ -19,34 +19,38 @@ function create(app: Elysia) {
   return app
 }
 
-export const ElysiaDynamicRoute = <E extends Elysia, const BasePath extends string = '', const NameFN extends string = "road">(app: E, config?: ElysiaConfig<BasePath, false> & ElysiaDynamicRouteConfig<NameFN>) => {
+export class ElysiaDynamicRoute<E extends Elysia, const BasePath extends string = '', const NameFN extends string = "road"> {
+  app: Elysia<BasePath, false>
+  constructor(app: E, public config?: ElysiaConfig<BasePath, false> & ElysiaDynamicRouteConfig<NameFN>) {
 
-  if (!app) throw new Error("Elysia instance is required")
+    if (!app) throw new Error("Elysia instance is required")
 
-  return app.use(new Elysia<BasePath>({ name: "elysia-dynamic-route", ...config })
-    .decorate(config?.init ?? "road", {
-      add: {
-        get<M extends "get">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
-          app.get(path, handler, hooks)
-          return create(app)
-        },
-        post<M extends "post">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
-          app.get(path, handler, hooks)
-          return create(app)
-        },
-        put<M extends "put">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
-          app.get(path, handler, hooks)
-          return create(app)
-        },
-        patch<M extends "patch">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
-          app.get(path, handler, hooks)
-          return create(app)
-        },
-        delete<M extends "delete">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
-          app.get(path, handler, hooks)
-          return create(app)
-        },
+    app.use(new Elysia<BasePath>({ name: "elysia-dynamic-route", ...config })
+      .decorate(config?.init ?? "road", {
+        add: {
+          get<M extends "get">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
+            app.get(path, handler, hooks)
+            return create(app)
+          },
+          post<M extends "post">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
+            app.get(path, handler, hooks)
+            return create(app)
+          },
+          put<M extends "put">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
+            app.get(path, handler, hooks)
+            return create(app)
+          },
+          patch<M extends "patch">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
+            app.get(path, handler, hooks)
+            return create(app)
+          },
+          delete<M extends "delete">(path: ElysiaDynamicRouteRode<M>['path'], handler: ElysiaDynamicRouteRode<M>['handler'], hooks?: ElysiaDynamicRouteRode<M>['hooks']) {
+            app.get(path, handler, hooks)
+            return create(app)
+          },
+        }
       }
-    }
-    ))
+      ))
+    this.app = app
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,23 @@
 import { describe, test, expect } from "bun:test"
 import Elysia from "elysia"
-import { ElysiaDynamicRouteRode } from "../src"
+import { ElysiaDynamicRoute } from "../src"
 
 
 describe("index", () => {
     test("should return true", async () => {
         const app = new Elysia()
-        const a = ElysiaDynamicRoute(app)
-            .get("/", async ({ road }) => {
-                road.add.get("/test", "test")
-                return "done"
-            })
+        new ElysiaDynamicRoute(app)
+        app.get("/", async ({ road }) => {
+            road.add.get("/test", "test")
+            return "done"
+        })
 
 
         const get1 = await app.handle(new Request("http://localhost")).then(res => res.text())
+        console.log("test  get1:", get1)
 
         const response = await app.handle(new Request("http://localhost/test")).then(res => res.text())
+        console.log("test  response:", response)
 
 
 
